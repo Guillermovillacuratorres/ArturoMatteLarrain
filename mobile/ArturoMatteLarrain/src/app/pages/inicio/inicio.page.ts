36c01faaf17d65036ee7ebc49e74f7a00ac7a69c
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2025-10-08 17:03:54 -0300

    menu

--- mobile/ArturoMatteLarrain/src/app/pages/inicio/inicio.page.ts
+++ mobile/ArturoMatteLarrain/src/app/pages/inicio/inicio.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {  IonButtons,
+          IonContent,
+          IonHeader,
+          IonMenu,
+          IonMenuButton,
+          IonTitle,
+          IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar,
+     CommonModule, FormsModule, IonMenu,IonMenuButton,
+     IonButtons
+    ]
 })
 export class InicioPage implements OnInit {
 
--- mobile/ArturoMatteLarrain/src/app/pages/login/login.page.ts
+++ mobile/ArturoMatteLarrain/src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader,
@@ -6,6 +6,7 @@ import { IonContent, IonHeader,
           IonInput, IonInputPasswordToggle,
           IonButton
   } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -20,9 +21,23 @@ import { IonContent, IonHeader,
 })
 export class LoginPage implements OnInit {
 
+  correo:string = "";
+  contrasena:string = "";
+
   constructor() { }
 
+  private router = inject(Router);
+
   ngOnInit() {
+  }
+
+
+  auth(){
+    if(this.correo == "duoc" && this.contrasena == "123"){
+      this.router.navigateByUrl("inicio");
+    }else{
+      alert("Credenciales incorrectas!!!");
+    }
   }
 
 }
