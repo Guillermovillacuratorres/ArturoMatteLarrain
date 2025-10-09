591b0a0e07c60b6b5cc6bbc6eb8813888fedd9db
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2025-10-08 21:20:41 -0300

    Clase 08-10-2025

--- mobile/ArturoMatteLarrain/src/app/pages/inicio/inicio.page.ts
+++ mobile/ArturoMatteLarrain/src/app/pages/inicio/inicio.page.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {  IonButtons,
-          IonContent,
-          IonHeader,
-          IonMenu,
-          IonMenuButton,
-          IonTitle,
-          IonToolbar,
-} from '@ionic/angular/standalone';
+
+
+import {IonicModule} from '@ionic/angular';
+import { addIcons } from 'ionicons';
+
+import { airplane, bluetooth, call, wifi } from 'ionicons/icons';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar,
-     CommonModule, FormsModule, IonMenu,IonMenuButton,
-     IonButtons
+  imports: [
+     CommonModule, FormsModule, IonicModule
     ]
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({ airplane, bluetooth, call, wifi });
+   }
 
   ngOnInit() {
   }
